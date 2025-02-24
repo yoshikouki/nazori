6e7dbc6f6f35c3fe55812487c6e328f0d3b0d79b
yoshikouki <yoshikouki@pepabo.com>
2025-02-24 17:52:24 +0900

    Fix dev error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-    ppr: true,
+    // [CanaryOnlyError: The experimental feature "experimental.ppr" can only be enabled when using the latest canary version of Next.js.]
+    ppr: false,
   },
 };
 
