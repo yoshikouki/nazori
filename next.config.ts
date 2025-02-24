89bd5ac34486e649b1368fbbe2b15c34f03045aa
yoshikouki <yoshikouki@pepabo.com>
2025-02-24 16:56:41 +0900

    Setup something

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+    ppr: true,
+  },
 };
 
 export default nextConfig;
