cd2ad55a7f5a3e17797a6d2ce889368b81ad2a29
yoshikouki <yoshikouki@pepabo.com>
2025-02-24 21:16:44 +0900

    Clean process

--- src/app/drawing-canvas.tsx
+++ src/app/drawing-canvas.tsx
@@ -1,59 +1,41 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useEffect, useRef } from "react";
+import { DownloadIcon, PencilLineIcon, Undo2Icon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 export const DrawingCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const isDrawingRef = useRef(false);
   const lastPosRef = useRef({ x: 0, y: 0 });
+  const [lineStyle, _setLineStyle] = useState({ width: 2, color: "#000" });
 
-  useEffect(() => {
+  const onPointerDown = (e: PointerEvent) => {
     if (!canvasRef.current) return;
-    const canvas = canvasRef.current;
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "#000";
+    e.preventDefault();
+    isDrawingRef.current = true;
+    lastPosRef.current = { x: e.offsetX, y: e.offsetY };
+    canvasRef.current.setPointerCapture(e.pointerId);
+  };
 
-    const handlePointerDown = (e: PointerEvent) => {
-      e.preventDefault();
-      isDrawingRef.current = true;
-      lastPosRef.current = { x: e.offsetX, y: e.offsetY };
-      canvas.setPointerCapture(e.pointerId);
-    };
+  const onPointerMove = (e: PointerEvent) => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx || !isDrawingRef.current) return;
+    ctx.beginPath();
+    ctx.moveTo(lastPosRef.current.x, lastPosRef.current.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastPosRef.current = { x: e.offsetX, y: e.offsetY };
+  };
 
-    const handlePointerMove = (e: PointerEvent) => {
-      e.preventDefault();
-      if (!isDrawingRef.current) return;
-      ctx.beginPath();
-      ctx.moveTo(lastPosRef.current.x, lastPosRef.current.y);
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
-      lastPosRef.current = { x: e.offsetX, y: e.offsetY };
-    };
+  const onPointerUp = (e: PointerEvent) => {
+    if (!canvasRef.current) return;
+    e.preventDefault();
+    isDrawingRef.current = false;
+    canvasRef.current.releasePointerCapture(e.pointerId);
+  };
 
-    const handlePointerUp = (e: PointerEvent) => {
-      e.preventDefault();
-      isDrawingRef.current = false;
-      canvas.releasePointerCapture(e.pointerId);
-    };
-
-    canvas.addEventListener("pointerdown", handlePointerDown);
-    canvas.addEventListener("pointermove", handlePointerMove);
-    canvas.addEventListener("pointerup", handlePointerUp);
-
-    return () => {
-      canvas.removeEventListener("pointerdown", handlePointerDown);
-      canvas.removeEventListener("pointermove", handlePointerMove);
-      canvas.removeEventListener("pointerup", handlePointerUp);
-    };
-  }, []);
-
-  const saveImage = () => {
+  const onSaveImage = () => {
     if (!canvasRef.current) return;
     const dataUrl = canvasRef.current.toDataURL("image/png");
     const link = document.createElement("a");
@@ -61,6 +43,33 @@ export const DrawingCanvas = () => {
     link.download = "drawing.png";
     link.click();
   };
+
+  // Initialize drawing canvas
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    canvas.addEventListener("pointerdown", onPointerDown);
+    canvas.addEventListener("pointermove", onPointerMove);
+    canvas.addEventListener("pointerup", onPointerUp);
+
+    return () => {
+      canvas.removeEventListener("pointerdown", onPointerDown);
+      canvas.removeEventListener("pointermove", onPointerMove);
+      canvas.removeEventListener("pointerup", onPointerUp);
+    };
+    // biome-ignore lint/correctness/useExhaustiveDependencies: React Compiler
+  }, [onPointerDown, onPointerMove, onPointerUp]);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+    ctx.lineWidth = lineStyle.width;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = lineStyle.color;
+  }, [lineStyle]);
 
   return (
     <div className="relative h-full w-full">
